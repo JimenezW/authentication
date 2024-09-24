6a8a88d9ade5e90653d3604f721c534adad36bb6
walter j <jimenezjimenezw@gmail.com>
2024-09-23 22:07:01 -0600

    Construccion de capas para el manejo del proyecto

--- src/main/java/com/authentication/controller/RoleController.java
+++ src/main/java/com/authentication/controller/RoleController.java
@@ -1,23 +1,40 @@
 package com.authentication.controller;
 
 import com.authentication.application.dto.RoleDTO;
+import com.authentication.application.service.RoleApplicationService;
 import com.authentication.application.service.RoleService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/roles")
 public class RoleController {
-    private final RoleService _roleService;
 
-    public RoleController(RoleService roleService) {
-        _roleService = roleService;
+    protected final RoleApplicationService roleService;
+
+    public RoleController(RoleApplicationService service) {
+        roleService = service;
+    }
+
+    @PostMapping
+    public RoleDTO createRole(@RequestBody RoleDTO roleDTO) {
+        return roleService.create(roleDTO);
+    }
+
+    @GetMapping("/{id}")
+    public RoleDTO getRoleById(@PathVariable Long id) {
+        return roleService.findById(id).orElseThrow(() -> new RuntimeException("Role not found"));
     }
 
     @GetMapping
-    public ResponseEntity<RoleDTO> test(){
-        return ResponseEntity.ok(_roleService.createRole());
+    public List<RoleDTO> getAllRoles() {
+        return roleService.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteRole(@PathVariable Long id) {
+        roleService.deleteById(id);
     }
 }
